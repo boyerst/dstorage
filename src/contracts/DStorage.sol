60f381ac80d33e0eff3662f40d14ff91ec430052
Steve Boyer <stephen.a.boyer@gmail.com>
2022-01-19 14:54:01 -0600

    Add contract mapping and struct

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
+
+  // State variable
+  string public name = 'DStorage';
+
   // Number of files
-  // Mapping fileId=>Struct 
+  // Mapping(fileId => Struct) 
+  mapping(uint => File) public files;
 
   // Struct
-
+  // Declare a struct type 'File' to use in our mapping
+    // Define data type, define attributes
+  struct File { 
+    uint fileId;
+    uint fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
   // Event
 
@@ -14,6 +29,14 @@ contract DStorage {
   }
 
   // Upload File function
+  // We pass the all the attributes
+
+
+
+    // How we would hardcode add a file to mapping
+    //files[1] = File(1, 'abc123', 1024, 'Foobar', 'Foo bar baz', 23423432, 0x0);
+
+
 
     // Make sure the file hash exists
 
@@ -33,5 +56,9 @@ contract DStorage {
     // Add File to the contract
 
     // Trigger an event
+
+  }
+
+
 
 }
