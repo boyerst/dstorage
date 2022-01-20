c8d003bb4681f37f0de0ad70faf26cbb4cf7778a
Steve Boyer <stephen.a.boyer@gmail.com>
2022-01-20 16:06:38 -0600

    Add requirements for file uploads

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -38,22 +38,25 @@ contract DStorage {
 
     // Add requirements for file uploads to ensure app doesn't break
     // Make sure the file hash exists
-
+      // Convert fileHash from string to bytes, read length
+    require(bytes(_fileHash).length > 0);
     // Make sure file type exists
-
+    require(bytes(_fileType).length > 0);
     // Make sure file description exists
-
+    require(bytes(_fileDescription).length > 0);
     // Make sure file fileName exists
-
+    require(bytes(_fileName).length > 0);
     // Make sure uploader address exists
-
+      // "Msg sender address is NOT equal to a zero address"
+    require(msg.sender!=address(0));
     // Make sure file size is more than 0
-
+    require(_fileSize>0);
 
 
     // Increment file id
-    // fileCount = fileCount +1;
+      // fileCount = fileCount +1;
     fileCount ++;
+
     // Add File to the contract
     // How we would hardcode uploadFile() to mapping
       //files[1] = File(1, 'abc123', 1024, 'Foobar', 'Foo bar baz', 23423432, 0x0);
@@ -65,14 +68,10 @@ contract DStorage {
       // With the counter, everytime we call this function the fileId will increment up
       // Declare uint fileCount with default value of 0 in state
       // Declare fileCount ++ above, because the first time we run through this function we want the fileCount to be 1 (from default 0)
-
     files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, 0x0);
-
 
     // Trigger an event
 
   }
-
-
 
 }
